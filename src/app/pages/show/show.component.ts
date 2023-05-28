74f3fbe7d8bdc90c3180a468dca9ef3217743836
Andres Negrette <andresdngtt@gmail.com>
2023-05-28 19:03:36 -0300

    Se crea maqueta de la vista update en elcomponente show

--- src/app/pages/show/show.component.ts
+++ src/app/pages/show/show.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.css']
+  styleUrls: ['./show.component.css'],
 })
-export class ShowComponent {
-
-}
+export class ShowComponent {}
