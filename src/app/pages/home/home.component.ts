c45501be76978790900a7abbda8ead57b0bdb3f6
Andres <andresdngtt@gmail.com>
2023-05-30 08:56:12 -0300

    Se conecta el header con la bd

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Materias } from 'src/app/models/materias.model';
+import { Usuario } from 'src/app/models/usuario.model';
 import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
@@ -9,11 +10,14 @@ import { CrudService } from 'src/app/services/crud.service';
 })
 export class HomeComponent implements OnInit {
   materias: Materias[] = [];
+  usuario: Usuario[] = [];
 
   constructor(private crudeService: CrudService) {}
 
   ngOnInit(): void {
     this.crudeService.getMaterias().subscribe((res: any) => {
+      this.usuario = [res.user];
+      console.log(this.usuario);
       this.materias = res.materias;
       console.log(this.materias);
     });
