c5a7709dc08d2c4f58b72bb538ecef7b7eae79c6
Andres <andresdngtt@gmail.com>
2023-06-17 00:26:14 -0300

    Se corrige error en carga de materias en el componente home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Materias } from 'src/app/models/materias.model';
 import { User } from 'src/app/models/user.model';
+import { CognitoService } from 'src/app/services/cognito.service';
 import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
@@ -14,14 +15,20 @@ export class HomeComponent implements OnInit {
   userName: string;
   isLoading: boolean = true;
 
-  constructor(private crudService: CrudService) {}
+  constructor(
+    private crudService: CrudService,
+    private cognitoService: CognitoService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-
     this.crudService.getMaterias().subscribe((materias: Materias[]) => {
       this.materias = materias;
       this.isLoading = false;
+    });
+
+    this.cognitoService.getUser().then(() => {
+      this.userName = sessionStorage.getItem('userName');
     });
   }
 }
